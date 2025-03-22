fcd0fc8c94deb24bc01b69bf97636981abcd8dfc
ayolamoiselab-dot <moiseayola4@gmail.com>
2025-03-22 19:43:07 +0000

    Remplace serviceAccountKey.json par variables d’environnement

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const admin = require('firebase-admin');
 const User = require('./models/UserModel');
 const CategorieQuiz = require('./models/CategorieQuizModel');
 const ResultatQuiz = require('./models/ResultatQuizModel');
-const serviceAccount = require('./config/serviceAccountKey.json');
+const serviceAccount = {
+  
+    "type": "service_account",
+    "project_id": "miawodo-c5be2",
+    "private_key_id": "cd1a59cdd4d9113ab5f4351f9992318ed342712e",
+    "private_key": "process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')", // Gère les retours à la ligne
+    "client_email": "firebase-adminsdk-fbsvc@miawodo-c5be2.iam.gserviceaccount.com",
+    "client_id": "117892011576494679572",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40miawodo-c5be2.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  
+};
 const cors = require('cors');
 const multer = require('multer');
 //const videoRoutes = require('./routes/videoRoutes'); // Si tu as un fichier séparé pour les routes
